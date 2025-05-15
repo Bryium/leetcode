ffea36bed81479c40313750b92e4c8f4536d57c6
Bryium <bryiumonyancha@gmail.com>
2025-05-15 14:47:27 +0300

    add another example usage

--- Hard/Candy.py
+++ Hard/Candy.py
@@ -27,3 +27,8 @@ if __name__ == "__main__":
   ratings = [1,2,2]
   result = solution.candy(ratings)
   print(result) 
+
+  #Example 3
+  ratings = [1, 3, 4, 5, 2]
+  result = solution.candy(ratings)
+  print(result) 
