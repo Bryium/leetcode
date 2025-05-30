46648d27ed4f2bdf8a1a9541a34fbe4b39accb76
Bryium <bryiumonyancha@gmail.com>
2025-05-30 18:23:01 +0300

    solve the two sum problem

---
+++ Easy/Two_Sum.py
@@ -0,0 +1,30 @@
+# This problem is about Finding two numbers in the list whose sum equals the target and return their indices.
+
+class Solution(object):
+    def twoSum(self, nums, target):
+
+      seen = {}
+
+      for i, num in enumerate(nums):
+        complement = target - num
+
+        if complement in seen:
+          return [seen[complement], i]
+
+        seen[num] = i
+
+# Example usage
+if __name__ == "__main__":
+    solution = Solution()
+
+    # Example 1
+    nums1 = [2, 7, 11, 15]
+    target1 = 9
+    result1 = solution.twoSum(nums1, target1)
+    print(result1)  
+
+    # Example 2
+    nums2 = [3, 2, 4]
+    target2 = 6
+    result2 = solution.twoSum(nums2, target2)
+    print(result2)  
