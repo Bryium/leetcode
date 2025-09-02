7f4da29d7a54eb765a9eb515f338ca463f658bb2
Bryium <bryiumonyancha@gmail.com>
2025-09-02 13:40:14 +0300

    solve maxprofit problem

---
+++ practise/max_profit.py
@@ -0,0 +1,23 @@
+def maxprofit(prices):
+  
+  left = 0
+  right = 1
+  max_diff = 0
+
+  while right < len(prices):
+    if prices[left] < prices[right]:
+      diff = prices[right] - prices[left]
+      max_diff = max(max_diff, diff)
+    else:
+      left = right
+    right += 1
+  return max_diff
+
+# Example usage:
+if __name__ == "__main__":
+  prices1 = [7,1,5,3,6,4]
+  print(maxprofit(prices1))  
+
+  prices2 = [7,6,4,3,1]
+  print(maxprofit(prices2))  
+    
